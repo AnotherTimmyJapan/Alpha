e8e2025b4c9321260b33a7c26c08d5cfaa9387f6
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-02-04 05:59:20 +0900

    group0.h

--- actors/group0.h
+++ actors/group0.h
@@ -103,4 +103,19 @@ extern const Gfx white_particle_small_dl_end[];
 extern const Gfx white_particle_small_dl[];
 extern const Gfx white_particle_small_unused_dl[];
 
+//mip!!!!
+extern Hierarchy RCP_rabbitHierarchy[];
+extern Gfx RCP_newrabbit_base0[];
+extern Gfx RCP_newrabbit_base1[];
+extern Gfx RCP_newrabbit_base2[];
+extern Gfx RCP_newrabbit_base3[];
+extern Gfx RCP_newrabbit_base4[];
+extern Gfx RCP_newrabbit_base5[];
+extern Gfx RCP_newrabbit_base6[];
+extern Gfx RCP_newrabbit_base7[];
+extern Gfx RCP_newrabbit_base8[];
+extern Gfx RCP_newrabbit_base9[];
+extern Gfx RCP_newrabbit_base10[];
+extern Gfx RCP_newrabbit_base11[];
+extern AnimePtr RCP_RabbitAnime[];
 #endif
