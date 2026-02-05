da374a9e879817a5caf2324711161cc18349914f
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-02-06 02:05:59 +0900

    interaction.c を更新

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -704,7 +704,9 @@ u32 interact_star_or_key(struct MarioState *m, UNUSED u32 interactType, struct O
 }
 
 u32 interact_flag(struct MarioState *m, UNUSED u32 interactType, struct Object *o) {
-u32 starIndex;
+    u32 starIndex;
+    u32 starGrabAction = ACT_STAR_DANCE_EXIT;
+    u32 noExit = (o->oInteractionSubtype & INT_SUBTYPE_NO_EXIT) != 0;
     if (m->health >= 0x100) {
         mario_stop_riding_and_holding(m);
 
